bf15060c68373fcab8539bda90556eacf77976c2
Jonathan Rochkind <jonathan@dnil.net>
2014-10-13 15:47:47 -0400

    touch up

--- lib/borrow_direct/find_item.rb
+++ lib/borrow_direct/find_item.rb
@@ -31,9 +31,9 @@ module BorrowDirect
     # Returns the actual complete BD response hash. You may want
     # #bd_requestable? instead
     #
-    #    finder.find(:isbn => "12345545456")
-    #    finder.find(:lccn => "12345545456")
-    #    finder.find(:oclc => "12345545456")
+    #    finder.find_item_request(:isbn => "12345545456")
+    #    finder.find_item_request(:lccn => "12345545456")
+    #    finder.find_item_request(:oclc => "12345545456")
     def find_item_request(options)
       search_type, search_value = nil, nil
       options.each_pair do |key, value|
@@ -49,33 +49,9 @@ module BorrowDirect
         raise ArgumentError.new("Missing valid search type and value: '#{options}'")
       end
 
-      type = options.keys.first.to_s.upcase
-      value = options.values.first
-
       request exact_search_request_hash(search_type, search_value)
     end
 
-    # need to send a key and value for a valid exact_search type
-    # type can be string or symbol, lowercase or uppercase. 
-    #
-    # Returns true or false -- can the item actually be requested
-    # via BorrowDirect. 
-    #
-    #    finder.bd_requestable? :isbn => "12345545456"
-    def bd_requestable?(options)
-      resp = find_item_request(options)
-
-      # Items that are available locally, and thus not requestable via BD, can
-      # only be found by looking at the RequestMessage, bah
-      h = resp["Item"]["RequestLink"]
-      if h && h["RequestMessage"] == "This item is available locally"
-        return false
-      end
-
-      return resp["Item"]["Available"].to_s == "true"
-    end
-
-    protected
 
     def exact_search_request_hash(type, value)
       {
