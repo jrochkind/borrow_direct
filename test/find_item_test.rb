71a4fddb53a6e11cd299d6d49580e5c6d3998cc9
Jonathan Rochkind <jonathan@dnil.net>
2014-10-14 13:34:44 -0400

    use global variables instead of constants so ruby don't complain. still not great. yeah, the vcr cassettes are a mess due to minitest-vcr bug

--- test/find_item_test.rb
+++ test/find_item_test.rb
@@ -7,12 +7,15 @@ require 'borrow_direct/find_item'
 VCRFilter.sensitive_data! :bd_library_symbol, :bd_finditem
 VCRFilter.sensitive_data! :bd_finditem_patron, :bd_finditem
 
-REQUESTABLE_ITEM_ISBN     = "9810743734" # item is in BD, and can be requested
-LOCALLY_AVAIL_ITEM_ISBN   = "0745649890"  # item is in BD, but is avail locally so not BD-requestable
-NOT_REQUESTABLE_ITEM_ISBN = "1441190090" # in BD, and we don't have it, but no libraries let us borrow (in this case, it's an ebook)
+$REQUESTABLE_ITEM_ISBN     = "9810743734" # item is in BD, and can be requested
+$LOCALLY_AVAIL_ITEM_ISBN   = "0745649890"  # item is in BD, but is avail locally so not BD-requestable
+$NOT_REQUESTABLE_ITEM_ISBN = "1441190090" # in BD, and we don't have it, but no libraries let us borrow (in this case, it's an ebook)
+
 
 
 describe "BorrowDirect::FindItem", :vcr => {:tag => :bd_finditem } do
+  
+
 
   describe "with defaults" do
     before do
@@ -69,11 +72,11 @@ describe "BorrowDirect::FindItem", :vcr => {:tag => :bd_finditem } do
 
 
   it "finds a requestable item" do
-    resp = BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).find_item_request(:isbn => REQUESTABLE_ITEM_ISBN)
+    resp = BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).find_item_request(:isbn => $REQUESTABLE_ITEM_ISBN)
   end
 
   it "finds a locally available item" do
-    resp = BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).find_item_request(:isbn => LOCALLY_AVAIL_ITEM_ISBN)
+    resp = BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).find_item_request(:isbn => $LOCALLY_AVAIL_ITEM_ISBN)
   end
 
   it "finds an item that does not exist in BD" do
@@ -82,11 +85,11 @@ describe "BorrowDirect::FindItem", :vcr => {:tag => :bd_finditem } do
 
   #describe "bd_requestable?" do
     it "says yes for requestable item" do
-      assert_equal true, BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).bd_requestable?(:isbn => REQUESTABLE_ITEM_ISBN)
+      assert_equal true, BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).bd_requestable?(:isbn => $REQUESTABLE_ITEM_ISBN)
     end
 
     it "says no for locally available item" do
-      assert_equal false, BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).bd_requestable?(:isbn => LOCALLY_AVAIL_ITEM_ISBN)
+      assert_equal false, BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).bd_requestable?(:isbn => $LOCALLY_AVAIL_ITEM_ISBN)
     end
 
     it "says no for item that does not exist in BD" do
@@ -94,7 +97,7 @@ describe "BorrowDirect::FindItem", :vcr => {:tag => :bd_finditem } do
     end
 
     it "says no for item that no libraries will lend" do
-      assert_equal false, BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).bd_requestable?(:isbn => NOT_REQUESTABLE_ITEM_ISBN)
+      assert_equal false, BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).bd_requestable?(:isbn => $NOT_REQUESTABLE_ITEM_ISBN)
     end
 
   #end
--- test/request_item_test.rb
+++ test/request_item_test.rb
@@ -9,9 +9,9 @@ require 'borrow_direct/request_item'
 VCRFilter.sensitive_data! :bd_library_symbol, :bd_requestitem
 VCRFilter.sensitive_data! :bd_finditem_patron, :bd_requestitem
 
-REQUESTABLE_ITEM_ISBN     = "9797994864" # item is in BD, and can be requested
-LOCALLY_AVAIL_ITEM_ISBN   = "0745649890"  # item is in BD, but is avail locally so not BD-requestable
-NOT_REQUESTABLE_ITEM_ISBN = "1441190090" # in BD, and we don't have it, but no libraries let us borrow (in this case, it's an ebook)
+$REQUESTABLE_ITEM_ISBN     = "9797994864" # item is in BD, and can be requested
+$LOCALLY_AVAIL_ITEM_ISBN   = "0745649890"  # item is in BD, but is avail locally so not BD-requestable
+$NOT_REQUESTABLE_ITEM_ISBN = "1441190090" # in BD, and we don't have it, but no libraries let us borrow (in this case, it's an ebook)
 
 
 
@@ -39,9 +39,9 @@ describe "BorrowDirect::RequestItem", :vcr => {:tag => :bd_requestitem } do
 
 
   it "raw requests an unrequestable item" do
-    auth_id = make_find_item_for_auth(:isbn => NOT_REQUESTABLE_ITEM_ISBN)
+    auth_id = make_find_item_for_auth(:isbn => $NOT_REQUESTABLE_ITEM_ISBN)
 
-    resp = BorrowDirect::RequestItem.new(auth_id).request_item_request(:isbn => NOT_REQUESTABLE_ITEM_ISBN)
+    resp = BorrowDirect::RequestItem.new(auth_id).request_item_request(:isbn => $NOT_REQUESTABLE_ITEM_ISBN)
 
 
     assert_present resp
@@ -50,25 +50,25 @@ describe "BorrowDirect::RequestItem", :vcr => {:tag => :bd_requestitem } do
 
   describe "make_request" do
     it "make_request for a requestable item" do
-      auth_id = make_find_item_for_auth(:isbn => REQUESTABLE_ITEM_ISBN)
+      auth_id = make_find_item_for_auth(:isbn => $REQUESTABLE_ITEM_ISBN)
 
-      request_id = BorrowDirect::RequestItem.new(auth_id).make_request(:isbn => REQUESTABLE_ITEM_ISBN)
+      request_id = BorrowDirect::RequestItem.new(auth_id).make_request(:isbn => $REQUESTABLE_ITEM_ISBN)
 
       assert_present request_id    
     end
 
     it "make_request for an unrequestable item" do
-      auth_id = make_find_item_for_auth(:isbn => NOT_REQUESTABLE_ITEM_ISBN)
+      auth_id = make_find_item_for_auth(:isbn => $NOT_REQUESTABLE_ITEM_ISBN)
 
-      resp = BorrowDirect::RequestItem.new(auth_id).make_request(:isbn => NOT_REQUESTABLE_ITEM_ISBN)
+      resp = BorrowDirect::RequestItem.new(auth_id).make_request(:isbn => $NOT_REQUESTABLE_ITEM_ISBN)
 
       assert_nil resp
     end
 
     it "make_request for a locally available item" do
-      auth_id = make_find_item_for_auth(:isbn => LOCALLY_AVAIL_ITEM_ISBN)
+      auth_id = make_find_item_for_auth(:isbn => $LOCALLY_AVAIL_ITEM_ISBN)
 
-      resp = BorrowDirect::RequestItem.new(auth_id).make_request(:isbn => LOCALLY_AVAIL_ITEM_ISBN)
+      resp = BorrowDirect::RequestItem.new(auth_id).make_request(:isbn => $LOCALLY_AVAIL_ITEM_ISBN)
 
       assert_nil resp
     end
@@ -76,18 +76,18 @@ describe "BorrowDirect::RequestItem", :vcr => {:tag => :bd_requestitem } do
 
   describe "make_request!" do
     it "returns number for succesful request" do
-      auth_id = make_find_item_for_auth(:isbn => REQUESTABLE_ITEM_ISBN)
+      auth_id = make_find_item_for_auth(:isbn => $REQUESTABLE_ITEM_ISBN)
 
-      request_id = BorrowDirect::RequestItem.new(auth_id).make_request!(:isbn => REQUESTABLE_ITEM_ISBN)
+      request_id = BorrowDirect::RequestItem.new(auth_id).make_request!(:isbn => $REQUESTABLE_ITEM_ISBN)
 
       assert_present request_id    
     end
 
     it "raises for unrequestable" do
-      auth_id = make_find_item_for_auth(:isbn => NOT_REQUESTABLE_ITEM_ISBN)
+      auth_id = make_find_item_for_auth(:isbn => $NOT_REQUESTABLE_ITEM_ISBN)
 
       error = assert_raises(BorrowDirect::Error) do
-        request_id = BorrowDirect::RequestItem.new(auth_id).make_request!(:isbn => NOT_REQUESTABLE_ITEM_ISBN)
+        request_id = BorrowDirect::RequestItem.new(auth_id).make_request!(:isbn => $NOT_REQUESTABLE_ITEM_ISBN)
       end
     end
     
@@ -97,7 +97,7 @@ describe "BorrowDirect::RequestItem", :vcr => {:tag => :bd_requestitem } do
   # Helper method to do the FindItem and get a BD AuthorizationID, sadly
   # neccesary first for making a request, really slowing things down yes. 
   def make_find_item_for_auth(conditions)
-    resp = BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).find_item_request(:isbn => REQUESTABLE_ITEM_ISBN)
+    resp = BorrowDirect::FindItem.new(VCRFilter[:bd_finditem_patron] , VCRFilter[:bd_library_symbol]).find_item_request(:isbn => $REQUESTABLE_ITEM_ISBN)
     auth_id = resp["Item"]["AuthorizationId"]
 
     assert ! auth_id.nil?, "No AuthorizationId received from BD"
