6de2e5164f09c03300a856c92e36a835f6cb4a36
Jonathan Rochkind <jonathan@dnil.net>
2014-10-15 13:55:18 -0400

    require all the things

--- lib/borrow_direct.rb
+++ lib/borrow_direct.rb
@@ -1,7 +1,12 @@
 require "borrow_direct/version"
 
 require 'borrow_direct/defaults'
+require 'borrow_direct/error'
+
 require 'borrow_direct/request'
+require 'borrow_direct/find_item'
+require 'borrow_direct/request_item'
+
 
 module BorrowDirect
 
