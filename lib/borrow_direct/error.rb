5a378f3ed3c422bbabb785629ca5cb9c982104fc
Jonathan Rochkind <jonathan@dnil.net>
2014-12-03 15:14:29 -0500

    Errors, save url that caused the error in the exception

--- lib/borrow_direct/error.rb
+++ lib/borrow_direct/error.rb
@@ -1,9 +1,10 @@
 module BorrowDirect
   class Error < StandardError
-    attr_reader :bd_code
+    attr_reader :bd_code, :request_url
 
-    def initialize(msg, bd_code = nil) 
+    def initialize(msg, bd_code = nil, request_url = nil) 
       @bd_code = bd_code
+      @request_url = request_url
       if @bd_code
         msg = "#{@bd_code}: #{msg}"
       end
